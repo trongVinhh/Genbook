c1a7f0eb8c4fe6d023f1f3332e9da4f0b7acf948
trongVinhh <lamtrongvinh2003@gmail.com>
2024-10-03 00:06:07 +0700

    [RELEASE] - [003]: Update CICD workflows and config services

--- profile-service/src/main/java/com/devteria/profile/repository/UserProfileRepository.java
+++ profile-service/src/main/java/com/devteria/profile/repository/UserProfileRepository.java
@@ -1,11 +1,11 @@
 package com.devteria.profile.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.neo4j.repository.Neo4jRepository;
 import org.springframework.stereotype.Repository;
 
 import com.devteria.profile.entity.UserProfile;
-
-import java.util.Optional;
 
 @Repository
 public interface UserProfileRepository extends Neo4jRepository<UserProfile, String> {
