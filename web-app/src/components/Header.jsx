ab5846f3ba35890cc08c0c5b9987b03503323141
trongVinhh <lamtrongvinh2003@gmail.com>
2024-10-04 22:03:13 +0700

    [RELEASE]: add config and fix some problem

--- web-app/src/components/Header.jsx
+++ web-app/src/components/Header.jsx
@@ -180,7 +180,7 @@ export default function Header() {
             height: "35px",
             borderRadius: 6,
           }}
-          src="/logo/devteria-logo.png"
+          src="/logo/genbook.png"
         ></Box>
       </IconButton>
       <Search>
